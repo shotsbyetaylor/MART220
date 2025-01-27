eff502d5a8f2de50166e076e1f5986efdd156f89
Taylor Denton <taylor.denton@umconnect.umt.edu>
2025-01-27 12:58:10 -0700

    Week 2

---
+++ Week2/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(1000, 1000);
+}
+function draw() {
+  background('steelblue');
+  // create some basic shapes after this circle
+  circle(35,55,25);
+  circle(85,55,25);
+  triangle(25,75,95,75,60,110);
+
+
+  fill(255, 255, 30);
+  triangle(100,100,100,400,800,250);
+  fill(300, 90, 30);
+  circle(140,210,40);
+  circle(340,320,30);
+  circle(440,250,50);
+  circle(240,240,40);
+  circle(440,280,20);
+  circle(540,220,20);
+  circle(640,240,40);
+  rect(100,50,100,400,125,100,125,400);
+
+  //pepperoni pizza
+  
+}
